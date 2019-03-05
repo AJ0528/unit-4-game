0c3fd5f26b0c1d629d8d879e14e64671c39f7362
Alfredo Torres <46428004+AJ0528@users.noreply.github.com>
2019-03-05 15:15:50 -0500

    Add files via upload

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+var randomResult;
+var loss;
+var win;
+
+for(var i = 0; i<4; i++){
+    var random = Math.floor(Math.random() * 10);
+    console.log("random");
+    var crystals = $("<div>");
+        crystal.attr("class", 'crystal');
+        }
+   
+ $(".crystals").append(crystal);
+    console.log("")
+});
+
+
+
+
+
+// speudo code 
+// game with 4 crystals 
+// needs to have random number
+// Every crystal needs a random number
+// needs click functions
+// crysal should add up
+
+console.log("hello world");
